7e1de028be2a748911fc2ba03b235c59abf66a5a
YaroslavBugayov <bugayov576@gmail.com>
2023-07-24 00:53:00 +0300

    project setting

--- app/src/main/java/com/bugayov/weatherapi/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/bugayov/weatherapi/presentation/ui/MainActivity.kt
@@ -1,33 +1,34 @@
 package com.bugayov.weatherapi.presentation.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.bugayov.weatherapi.R
-import com.bugayov.weatherapi.databinding.ActivityMainBinding
 import com.bugayov.weatherapi.presentation.viewmodels.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     private val vm by viewModel<MainViewModel>()
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContent {
+            Text(text = "Hello world")
+        }
 
         vm.weather.observe(this, Observer{
-            binding.textLocation.text = getString(R.string.location_output, it.location)
-            binding.textCondition.text = getString(R.string.condition_output, it.condition)
-            binding.textTemperature.text = getString(R.string.temperature_output, it.temperature)
-            binding.textWindSpeed.text = getString(R.string.windSpeed_output, it.windSpeed)
-            binding.textCloudiness.text = getString(R.string.cloudiness_output, it.cloudiness)
-            binding.imageView.setImageBitmap(it.conditionImage)
+//            binding.textLocation.text = getString(R.string.location_output, it.location)
+//            binding.textCondition.text = getString(R.string.condition_output, it.condition)
+//            binding.textTemperature.text = getString(R.string.temperature_output, it.temperature)
+//            binding.textWindSpeed.text = getString(R.string.windSpeed_output, it.windSpeed)
+//            binding.textCloudiness.text = getString(R.string.cloudiness_output, it.cloudiness)
+//            binding.imageView.setImageBitmap(it.conditionImage)
         })
 
         vm.error.observe(this, Observer{ error ->
@@ -38,9 +39,20 @@ class MainActivity : AppCompatActivity() {
 
         vm.updateWeather()
 
-        binding.setLocationButton.setOnClickListener {
-            vm.updateWeather(binding.editText.text.toString())
-        }
+//        binding.setLocationButton.setOnClickListener {
+//            vm.updateWeather(binding.editText.text.toString())
+//        }
+    }
+
+    @Composable
+    fun WeatherCard(city: String) {
+        Text(city)
+    }
+
+    @Preview
+    @Composable
+    fun PreviewWeatherCard() {
+        WeatherCard(city = "London")
     }
 
 }
