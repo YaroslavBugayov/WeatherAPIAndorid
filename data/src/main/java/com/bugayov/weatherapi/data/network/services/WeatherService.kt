fd8aa71dd8cdee46444f14b5d4c9be73c8ac42d1
YaroslavBugayov <bugayov576@gmail.com>
2023-07-23 18:54:39 +0300

    api key

--- data/src/main/java/com/bugayov/weatherapi/data/network/services/WeatherService.kt
+++ data/src/main/java/com/bugayov/weatherapi/data/network/services/WeatherService.kt
@@ -1,15 +1,13 @@
 package com.bugayov.weatherapi.data.network.services
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.util.Log
+import com.bugayov.data.BuildConfig
 import com.bugayov.weatherapi.data.network.api.WeatherApi
 import com.bugayov.weatherapi.data.network.models.WeatherResponse
 import com.bugayov.weatherapi.domain.utils.Resource
 import retrofit2.Retrofit
 import retrofit2.awaitResponse
 import retrofit2.converter.gson.GsonConverterFactory
-import java.lang.RuntimeException
 
 class WeatherService {
     private val retrofit: Retrofit = Retrofit.Builder()
@@ -22,7 +20,7 @@ class WeatherService {
     suspend fun getCurrentWeather(city: String): Resource<WeatherResponse> {
         val call = apiService.getCurrentWeather(
             city = city,
-            apiKey = "7dc565aa823fc68a9efee11052595cb9",
+            apiKey = BuildConfig.API_KEY,
             units = "metric"
         )
         Log.d("Request", call.request().toString())
